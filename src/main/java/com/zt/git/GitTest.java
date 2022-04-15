c633a96b64ea8b0875bf177e8e9e6b479e3bb2ae
zhangteng <1911805190@qq.com>
2022-04-15 15:40:40 +0800

    push commit2

--- src/main/java/com/zt/git/GitTest.java
+++ src/main/java/com/zt/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("44444");
         System.out.println("master");
         System.out.println("hot fix222");
+        System.out.println("push test");
     }
 }
